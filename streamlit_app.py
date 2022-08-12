74e240d32e4293cbcd6c5c85b5ece814272f4ce7
SudharsanGovindarajan <110613920+SudharsanGovindarajan@users.noreply.github.com>
2022-08-12 19:28:59 +0530

    Update streamlit_app.py
    Added Dinner diet menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,10 @@
 import streamlit
-streamlit.title('This is my first streamlit_app file')
+streamlit.title('Our diet menu')
+streamlit.header('Dinner')
+streamlit.text('Monday: Puttu')
+streamlit.text('Tuesday: Solakarudhu Salad')
+streamlit.text('Wednesday: Karuppu Arisi Kanji')
+streamlit.text('Thursday: Sundal')
+streamlit.text('Friday: Aval Sundal')
+streamlit.text('Saturday: Fruit Salad after evening snacks')
+streamlit.text('Sunday: Anything based on mood')
